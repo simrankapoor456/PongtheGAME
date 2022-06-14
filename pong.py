bfd4b72e058ed4544cd8dd998ee72a70cbcd0d07
Simran Kapoor <simrankapoor456@gmail.com>
2022-06-14 17:58:22 +0530

    paddle and the ball

--- pong.py
+++ pong.py
@@ -6,6 +6,13 @@ gamewindow.bgcolor("darkblue")
 gamewindow.setup(width=800, height=500)
 gamewindow.tracer(0) #stops the window from updating, so we've to manually update the game, this helps us to speed up the game
 
+#Paddle A
+
+
+#Paddle B
+
+
+#The Ball
 
 #Main Game Loop
 while True:
