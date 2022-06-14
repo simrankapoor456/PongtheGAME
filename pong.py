ed8053921f50abeea119271547558125ae9bcf3f
Simran Kapoor <simrankapoor456@gmail.com>
2022-06-14 17:56:53 +0530

    Pong the GAME

--- pong.py
+++ pong.py
@@ -6,9 +6,7 @@ gamewindow.bgcolor("darkblue")
 gamewindow.setup(width=800, height=500)
 gamewindow.tracer(0) #stops the window from updating, so we've to manually update the game, this helps us to speed up the game
 
-#checking if everything is working fine or not
 
-print("Testing")
 #Main Game Loop
 while True:
     gamewindow.update()
