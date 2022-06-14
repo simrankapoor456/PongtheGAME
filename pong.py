30b726ac2dc92763f57cc082600fd4f06f202ec3
Simran Kapoor <56875464+simrankapoor456@users.noreply.github.com>
2022-06-14 17:46:56 +0530

    The Pong Game

---
+++ pong.py
@@ -0,0 +1,13 @@
+import turtle
+
+gamewindow = turtle.Screen()
+gamewindow.title("Pong the GAME")
+gamewindow.bgcolor("darkblue")
+gamewindow.setup(width=800, height=500)
+gamewindow.tracer(0) #stops the window from updating, so we've to manually update the game, this helps us to speed up the game
+
+
+
+#Main Game Loop
+while True:
+    gamewindow.update()
